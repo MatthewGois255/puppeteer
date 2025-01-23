e74201e6a9afe4ee3e93c8240466f119d11749eb
Matheus Gois <matheusgois.pessoal@gmail.com>
2025-01-23 12:38:39 -0300

    agora vai

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const puppeteer = require("puppeteer");
     const page = await browser.newPage();
     await page.goto('https://wol.jw.org/pt/wol/h/r5/lp-t');
     new Promise (r => setTimeout(r, 1000));
-    await page.click('.lnc-button.lnc-button--primary.lnc-acceptCookiesButton');
     const texto = await page.evaluate(() => {
         return document.querySelector('.themeScrp').textContent;
     });
